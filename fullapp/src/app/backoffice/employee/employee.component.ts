d6fff68d5c11f7543ba378391b9d64e581dc23a2
Aina Rmngst <ainarmngst@gmail.com>
2024-02-21 12:14:43 +0300

    Server and DB config

--- fullapp/src/app/backoffice/employee/employee.component.ts
+++ fullapp/src/app/backoffice/employee/employee.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-employee',
--- fullapp/src/app/backoffice/product/product.component.ts
+++ fullapp/src/app/backoffice/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @Component({
   selector: 'app-product',
@@ -6,4 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
+  constructor(private _dialog: MatDialog){}
+  openAddEmpForm(){
+    this._dialog.open(AddProductComponent);
+  }
 }
